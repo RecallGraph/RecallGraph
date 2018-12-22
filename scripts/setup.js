8fe38f24514f3a267f284ad97ac02587e86975c5
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2018-12-22 20:31:35 +0530

    Added scripts.

---
+++ scripts/setup.js
@@ -0,0 +1,22 @@
+'use strict';
+const db = require('@arangodb').db;
+const documentCollections = ['_events', '_snapshots'];
+const edgeCollections = ['_commands', '_snapshot_link', '_evt_ss_link'];
+
+for (const localName of documentCollections) {
+  const qualifiedName = module.context.collectionName(localName);
+  if (!db._collection(qualifiedName)) {
+    db._createDocumentCollection(qualifiedName);
+  } else if (module.context.isProduction) {
+    console.debug(`collection ${qualifiedName} already exists. Leaving it untouched.`)
+  }
+}
+
+for (const localName of edgeCollections) {
+  const qualifiedName = module.context.collectionName(localName);
+  if (!db._collection(qualifiedName)) {
+    db._createEdgeCollection(qualifiedName);
+  } else if (module.context.isProduction) {
+    console.debug(`collection ${qualifiedName} already exists. Leaving it untouched.`)
+  }
+}
