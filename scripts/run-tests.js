cc0e34b09683abbcdf7ed738dc3b87989b7ce8f1
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2019-03-28 17:47:02 +0530

    Added `grep` support.

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,27 +1,50 @@
 'use strict';
-const mocha = require('@arangodb/foxx/mocha');
-const manager = require('@arangodb/foxx/manager');
-const { get, clone } = require('lodash');
 
-const manifestTests = clone(module.context.manifest.tests);
+const manager = require('@arangodb/foxx/manager');
+const { get } = require('lodash');
+const fs = require('fs');
+const Minimatch = require('minimatch').Minimatch;
+const isWindows = require('internal').platform.substr(0, 3) === 'win';
+const mocha = require('@arangodb/mocha');
 
 const argv = module.context.argv;
 const tests = get(argv, [0, 'tests']);
 const reporter = get(argv, [0, 'reporter'], 'default');
+const grep = get(argv, [0, 'grep']);
 
 const service = manager.lookupService(module.context.mount);
-if (tests) {
-  service.manifest.tests = tests;
+const testFiles = findTestFiles(service, tests);
+
+const result = mocha.run((file, context) => service.run(file, { context: context }), testFiles, reporter, grep);
+if (reporter === 'xunit' && Array.isArray(result) && result[1]) {
+  result[1].name = service.mount;
 }
 
-let results;
-try {
-  results = mocha.run(service, reporter);
-}
-catch (e) {
-  results = e.message;
+module.exports = result;
+
+function isNotPattern(pattern) {
+  return pattern.indexOf('*') === -1;
 }
 
-module.exports = results;
-
-service.manifest.tests = manifestTests;
+function findTestFiles(service, tests) {
+  const patterns = tests || service.manifest.tests || [];
+  if (patterns.every(isNotPattern)) {
+    return patterns.slice();
+  }
+  const basePath = fs.join(service.root, service.path);
+  const paths = fs.listTree(basePath);
+  const matchers = patterns.map((pattern) => {
+    if (pattern.charAt(0) === '/') {
+      pattern = pattern.slice(1);
+    }
+    else if (pattern.charAt(0) === '.' && pattern.charAt(1) === '/') {
+      pattern = pattern.slice(2);
+    }
+    return new Minimatch(pattern);
+  });
+  return paths.filter(
+    (path) => path && matchers.some((pattern) => pattern.match(
+      isWindows ? path.replace(/\\/g, '/') : path
+    )) && fs.isFile(fs.join(basePath, path))
+  );
+}
