c6d827b649003fb9cba8fa715905e3e3afe141e0
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-02-10 06:30:17 +0530

    Changed env var name to match conventions.

--- test/travis/run.sh
+++ test/travis/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 cd /mnt/evstore
-echo "evpasswd" |foxx run -u evuser -P -D evdb /evstore runTests \
-"{\"files\": ${EVTEST_FILES//"/\\"}, \"reporter\": \"suite\", \"grep\": \"$GREP\"}" \
-|./test/travis/parseResult.js
+echo "evpasswd" | foxx run -u evuser -P -D evdb /evstore runTests \
+  "{\"files\": ${FILES//"/\\"/}, \"reporter\": \"suite\", \"grep\": \"$GREP\"}" |
+  ./test/travis/parseResult.js
