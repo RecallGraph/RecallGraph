a841afa2700ecbf68c0683a03e053ee3db4a19d7
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-04-14 19:58:45 +0530

    [skip travis] Tracer integration (complete, untested).

--- main.js
+++ main.js
@@ -3,11 +3,12 @@
 const createRouter = require('@arangodb/foxx/router')
 const path = require('path')
 const fs = require('fs')
-const tracerMW = require('./lib/middleware/tracer')
-const { utils: { setEndpointTraceHeaders, initTracer } } = require('foxx-tracing')
+const {
+  utils: { setEndpointTraceHeaders, initTracer }, middleware
+} = require('foxx-tracing')
 
 initTracer()
-module.context.use(tracerMW)
+module.context.use(middleware)
 
 const router = createRouter()
 const routeBase = `${__dirname}/lib/routes`
