fbda6ed4a9245b607640a73fced6ec7663fa51e7
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2020-04-15 11:53:22 +0530

    [skip travis] Tracer integration (complete, untested).

--- test/helpers/init.js
+++ test/helpers/init.js
@@ -1,11 +1,9 @@
 'use strict'
 
 const { db } = require('@arangodb')
-
 const { merge, forEach, noop, omit } = require('lodash')
 const { SERVICE_COLLECTIONS } = require('../../lib/helpers')
 const loadSampleData = require('./loadSampleData')
-
 const cache = require('@arangodb/aql/cache')
 
 cache.properties({ mode: 'on' })
