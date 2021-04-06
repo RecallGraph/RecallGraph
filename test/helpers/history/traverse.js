f1a44481b3ea6ec6a907cf2c9b6f32b4e83cb477
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2021-04-06 19:03:04 +0530

    Moved cytoscape from dependencies to devDependencies (now required for tests only).

--- test/helpers/history/traverse.js
+++ test/helpers/history/traverse.js
@@ -3,7 +3,8 @@
 const { expect } = require('chai')
 const { db, aql } = require('@arangodb')
 const request = require('@arangodb/request')
-const { chain, sample, memoize, omit, isObject, pick, isEmpty } = require('lodash')
+const { chain, sample, memoize, omit, isObject, pick, isEmpty, map, remove, cloneDeep, zipObject, stubTrue } = require(
+  'lodash')
 const init = require('../util/init')
 const show = require('../../../lib/operations/show')
 const { cartesian, generateFilters } = require('../util')
@@ -12,11 +13,231 @@ const { traverse: traverseHandler } = require('../../../lib/handlers/traverseHan
 const { getCollectionType } = require('../../../lib/helpers')
 const { DOC_ID_REGEX } = require('../../../lib/constants')
 const {
-  traverseSkeletonGraph, createNodeBracepath, removeFreeEdges, buildFilteredGraph
+  traverseSkeletonGraph, createNodeBracepath
 } = require('../../../lib/operations/traverse/helpers')
+const cytoscape = require('cytoscape')
+const { parseExpr } = require('../../../lib/operations/helpers')
 
 const { baseUrl } = module.context
 const lineageCollName = module.context.collectionName('test_lineage')
+
+// Private
+function removeFreeEdges (vertices, edges) {
+  const vSet = new Set(map(vertices, '_id'))
+
+  remove(edges, e => [e._from, e._to].some(vid => !vSet.has(vid)))
+}
+
+function edgeIsValid (v, e, edgeCollections) {
+  const eColl = e.id().split('/')[0]
+
+  switch (edgeCollections[eColl]) {
+    case 'inbound':
+      return e.source().same(v)
+    case 'outbound':
+      return e.target().same(v)
+    default:
+      return true
+  }
+}
+
+function visitIsValid (traversal, uniqueVertices, uniqueEdges, edgeCollections, v, e, currentPath) {
+  let visitIsValid = (e === null) || edgeIsValid(v, e, edgeCollections)
+
+  if (visitIsValid) {
+    switch (uniqueVertices) {
+      case 'global':
+        visitIsValid = !traversal.gSet.has(v.id())
+        break
+
+      case 'path':
+        visitIsValid = !currentPath.vSet.has(v.id())
+        break
+    }
+  }
+
+  if (visitIsValid && uniqueEdges === 'path') {
+    visitIsValid = (e === null) || !currentPath.eSet.has(e.id())
+  }
+
+  return visitIsValid
+}
+
+function addVisit (traversal, minDepth, v, e, currentDepth, currentPath, vFilterFn, eFilterFn, pFilterFn) {
+  const path = cloneDeep(currentPath)
+  const vData = omit(v.data(), 'id')
+
+  traversal.gSet.add(v.id())
+  path.vertices.push(vData)
+  path.vSet.add(v.id())
+
+  let eData = null
+  if (e) {
+    eData = omit(e.data(), 'id', 'source', 'target')
+    path.edges.push(eData)
+    path.eSet.add(e.id())
+  }
+
+  if (currentDepth >= minDepth && vFilterFn(vData) && (!eData || eFilterFn(eData)) && pFilterFn(path)) {
+    traversal.vSet.add(v.id())
+    traversal.paths.push(path)
+
+    if (e) {
+      traversal.eSet.add(e.id())
+    }
+  }
+
+  return path
+}
+
+function getNeighborhood (v) {
+  return v.neighborhood().reduce((acc, el) => {
+    if (!acc) {
+      acc = {}
+    }
+
+    if (el.isNode()) {
+      acc[el.id()] = {
+        vertex: el,
+        edges: []
+      }
+    } else {
+      const groupNode = el.connectedNodes(`[id != '${v.id()}']`)
+      acc[groupNode.id()].edges.push(el)
+    }
+
+    return acc
+  })
+}
+
+function depthFirstWalk (traversal, minDepth, maxDepth, uniqueVertices, uniqueEdges, edgeCollections, vFilterFn,
+  eFilterFn, pFilterFn, v, e = null, currentDepth = 0,
+  currentPath = { vertices: [], edges: [], vSet: new Set(), eSet: new Set() }) {
+  if (visitIsValid(traversal, uniqueVertices, uniqueEdges, edgeCollections, v, e, currentPath)) {
+    const path = addVisit(traversal, minDepth, v, e, currentDepth, currentPath, vFilterFn, eFilterFn, pFilterFn)
+
+    if (currentDepth < maxDepth) {
+      const nbd = getNeighborhood(v)
+
+      for (const key in nbd) {
+        const group = nbd[key]
+        const nv = group.vertex
+
+        for (const ne of group.edges) {
+          depthFirstWalk(traversal, minDepth, maxDepth, uniqueVertices, uniqueEdges, edgeCollections, vFilterFn,
+            eFilterFn, pFilterFn, nv, ne, currentDepth + 1, path)
+        }
+      }
+    }
+  }
+}
+
+function breadthFirstWalk (traversal, minDepth, maxDepth, uniqueVertices, uniqueEdges, edgeCollections, vFilterFn,
+  eFilterFn, pFilterFn, nbds, currentDepth = 0) {
+  const childNbds = []
+
+  for (const nbd of nbds) {
+    const { groups, path: currentPath } = nbd
+
+    for (const key in groups) {
+      const group = groups[key]
+      const v = group.vertex
+
+      for (const e of group.edges) {
+        if (visitIsValid(traversal, uniqueVertices, uniqueEdges, edgeCollections, v, e, currentPath)) {
+          const path = addVisit(traversal, minDepth, v, e, currentDepth, currentPath, vFilterFn, eFilterFn, pFilterFn)
+
+          if (currentDepth < maxDepth) {
+            childNbds.push({
+              path,
+              groups: getNeighborhood(v)
+            })
+          }
+        }
+      }
+    }
+  }
+
+  if (currentDepth < maxDepth && childNbds.length) {
+    breadthFirstWalk(traversal, minDepth, maxDepth, uniqueVertices, uniqueEdges, edgeCollections, vFilterFn,
+      eFilterFn,
+      pFilterFn, childNbds, currentDepth + 1)
+  }
+}
+
+function buildFilteredGraph (svid, vertices, edges, minDepth, maxDepth, bfs, uniqueVertices, uniqueEdges,
+  edgeCollections, vFilter, eFilter, pFilter) {
+  const cy = cytoscape()
+
+  cy.startBatch()
+
+  cy.add(vertices.map(v => ({
+    group: 'nodes',
+    data: Object.assign({
+      id: v._id
+    }, v)
+  })))
+
+  cy.add(edges.map(e => ({
+    group: 'edges',
+    data: Object.assign({
+      id: e._id,
+      source: e._from,
+      target: e._to
+    }, e)
+  })))
+
+  cy.endBatch()
+
+  const vMap = zipObject(map(vertices, '_id'), vertices)
+  const eMap = zipObject(map(edges, '_id'), edges)
+  const traversal = {
+    gSet: new Set(),
+    vSet: new Set(),
+    eSet: new Set(),
+    paths: []
+  }
+  const sv = cy.$id(svid)
+  const vFilterFn = vFilter ? parseExpr(vFilter) : stubTrue
+  const eFilterFn = eFilter ? parseExpr(eFilter) : stubTrue
+  const pFilterFn = pFilter ? parseExpr(pFilter) : stubTrue
+
+  if (uniqueVertices === 'global') {
+    bfs = true
+  }
+
+  if (bfs) {
+    breadthFirstWalk(traversal, minDepth, maxDepth, uniqueVertices, uniqueEdges, edgeCollections, vFilterFn,
+      eFilterFn,
+      pFilterFn, [
+        {
+          path: { vertices: [], edges: [], vSet: new Set(), eSet: new Set() },
+          groups: {
+            [sv.id()]: {
+              vertex: sv,
+              edges: [null]
+            }
+          }
+        }
+      ])
+  } else {
+    depthFirstWalk(traversal, minDepth, maxDepth, uniqueVertices, uniqueEdges, edgeCollections, vFilterFn, eFilterFn,
+      pFilterFn, sv)
+  }
+
+  traversal.vertices = Array.from(traversal.vSet).map(id => vMap[id])
+  traversal.edges = Array.from(traversal.eSet).map(id => eMap[id])
+
+  delete traversal.gSet
+  delete traversal.vSet
+  delete traversal.eSet
+  for (const path of traversal.paths) {
+    delete path.vSet
+    delete path.eSet
+  }
+
+  return traversal
+}
 
 // Public
 const generateCombos = memoize((keys = [], include = true, {
